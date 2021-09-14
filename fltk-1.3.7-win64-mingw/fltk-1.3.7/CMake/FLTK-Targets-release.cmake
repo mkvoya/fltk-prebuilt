6fcbdfea636a1d91619343d7b1e791b688ec69f1
Mingkai Dong <mingkaidong@gmail.com>
2021-09-14 19:41:21 +0800

    add prebuilt fltk-1.3.7-win64-mingw

---
+++ fltk-1.3.7-win64-mingw/fltk-1.3.7/CMake/FLTK-Targets-release.cmake
@@ -0,0 +1,158 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "fltk_z" for configuration "Release"
+set_property(TARGET fltk_z APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_z PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_z.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_z )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_z "${_IMPORT_PREFIX}/lib/libfltk_z.a" )
+
+# Import target "fltk_z_SHARED" for configuration "Release"
+set_property(TARGET fltk_z_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_z_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_z.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_z.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_z_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_z_SHARED "${_IMPORT_PREFIX}/lib/libfltk_z.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_z.dll" )
+
+# Import target "fltk_jpeg" for configuration "Release"
+set_property(TARGET fltk_jpeg APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_jpeg PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_jpeg.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_jpeg )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_jpeg "${_IMPORT_PREFIX}/lib/libfltk_jpeg.a" )
+
+# Import target "fltk_jpeg_SHARED" for configuration "Release"
+set_property(TARGET fltk_jpeg_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_jpeg_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_jpeg.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_jpeg.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_jpeg_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_jpeg_SHARED "${_IMPORT_PREFIX}/lib/libfltk_jpeg.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_jpeg.dll" )
+
+# Import target "fltk_png" for configuration "Release"
+set_property(TARGET fltk_png APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_png PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_png.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_png )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_png "${_IMPORT_PREFIX}/lib/libfltk_png.a" )
+
+# Import target "fltk_png_SHARED" for configuration "Release"
+set_property(TARGET fltk_png_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_png_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_png.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_png.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_png_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_png_SHARED "${_IMPORT_PREFIX}/lib/libfltk_png.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_png.dll" )
+
+# Import target "fluid" for configuration "Release"
+set_property(TARGET fluid APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fluid PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/fluid.exe"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fluid )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fluid "${_IMPORT_PREFIX}/bin/fluid.exe" )
+
+# Import target "fltk" for configuration "Release"
+set_property(TARGET fltk APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk "${_IMPORT_PREFIX}/lib/libfltk.a" )
+
+# Import target "fltk_forms" for configuration "Release"
+set_property(TARGET fltk_forms APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_forms PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_forms.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_forms )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_forms "${_IMPORT_PREFIX}/lib/libfltk_forms.a" )
+
+# Import target "fltk_images" for configuration "Release"
+set_property(TARGET fltk_images APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_images PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_images.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_images )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_images "${_IMPORT_PREFIX}/lib/libfltk_images.a" )
+
+# Import target "fltk_gl" for configuration "Release"
+set_property(TARGET fltk_gl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_gl PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_gl.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_gl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_gl "${_IMPORT_PREFIX}/lib/libfltk_gl.a" )
+
+# Import target "fltk_SHARED" for configuration "Release"
+set_property(TARGET fltk_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_SHARED "${_IMPORT_PREFIX}/lib/libfltk.dll.a" "${_IMPORT_PREFIX}/bin/libfltk.dll" )
+
+# Import target "fltk_forms_SHARED" for configuration "Release"
+set_property(TARGET fltk_forms_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_forms_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_forms.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_forms.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_forms_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_forms_SHARED "${_IMPORT_PREFIX}/lib/libfltk_forms.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_forms.dll" )
+
+# Import target "fltk_images_SHARED" for configuration "Release"
+set_property(TARGET fltk_images_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_images_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_images.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_images.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_images_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_images_SHARED "${_IMPORT_PREFIX}/lib/libfltk_images.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_images.dll" )
+
+# Import target "fltk_gl_SHARED" for configuration "Release"
+set_property(TARGET fltk_gl_SHARED APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fltk_gl_SHARED PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libfltk_gl.dll.a"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libfltk_gl.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS fltk_gl_SHARED )
+list(APPEND _IMPORT_CHECK_FILES_FOR_fltk_gl_SHARED "${_IMPORT_PREFIX}/lib/libfltk_gl.dll.a" "${_IMPORT_PREFIX}/bin/libfltk_gl.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
